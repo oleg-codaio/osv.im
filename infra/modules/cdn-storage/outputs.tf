29c67f8dcdc70d1464ace9b6206e6a77da618a92
Oleg Vaskevich <oleg@osv.im>
2019-01-08 11:28:43 -0800

    Grant invalidate permissions to CI user

--- infra/modules/cdn-storage/outputs.tf
+++ infra/modules/cdn-storage/outputs.tf
@@ -3,6 +3,11 @@ output "bucket_arn" {
   value       = "${aws_s3_bucket.root.arn}"
 }
 
+output "cloudfront_arn" {
+  description = "The ARN of the created CloudFront distribution"
+  value       = "${aws_cloudfront_distribution.root.arn}"
+}
+
 output "cloudfront_id" {
   description = "The ID of the created CloudFront distribution"
   value       = "${aws_cloudfront_distribution.root.id}"
