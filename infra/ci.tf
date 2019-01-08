be488595ce7a7c8130e720e59af30c141bfb82e9
Oleg Vaskevich <oleg@osv.im>
2019-01-08 11:39:11 -0800

    Grant invalidate permissions to CI user (#15)
    * Grant invalidate permissions to CI user
    
    * Make Drone happy

--- infra/ci.tf
+++ infra/ci.tf
@@ -12,6 +12,12 @@ locals {
     "${module.legal_cdn_storage.bucket_arn}",
     "${module.globetheater_cdn_storage.bucket_arn}",
   ]
+
+  ci_write_cdns = [
+    "${module.root_cdn_storage.cloudfront_arn}",
+    "${module.legal_cdn_storage.cloudfront_arn}",
+    "${module.globetheater_cdn_storage.cloudfront_arn}",
+  ]
 }
 
 resource "aws_iam_policy" "ci_bucket_write_policy" {
@@ -37,7 +43,13 @@ data "aws_iam_policy_document" "ci_s3_policy" {
     resources = ["${concat(local.ci_write_buckets, formatlist("%s/*", local.ci_write_buckets))}"]
   }
 
-  # TODO: add permission to invalidate distributions.
+  statement = {
+    sid = "AllowInvalidateCdn"
+
+    actions = ["cloudfront:CreateInvalidation"]
+
+    resources = ["${local.ci_write_cdns}"]
+  }
 }
 
 resource "aws_iam_user_policy_attachment" "ci_bucket_write_policy_attachment" {
