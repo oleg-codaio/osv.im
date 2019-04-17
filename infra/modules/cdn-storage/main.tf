038fdaf30d3f24b9842f961a337173be10859326
Oleg Vaskevich <oleg@osv.im>
2019-04-17 01:38:34 -0700

    Relax health checks

--- infra/modules/cdn-storage/main.tf
+++ infra/modules/cdn-storage/main.tf
@@ -135,7 +135,7 @@ resource "aws_route53_health_check" "root" {
   port              = 443
   measure_latency   = true
   request_interval  = 30
-  failure_threshold = 3
+  failure_threshold = 2
   enable_sni        = true
 
   tags {
@@ -151,7 +151,7 @@ resource "aws_cloudwatch_metric_alarm" "health" {
   evaluation_periods        = "1"
   metric_name               = "HealthCheckStatus"
   namespace                 = "AWS/Route53"
-  period                    = "60"
+  period                    = "600"
   statistic                 = "Minimum"
   threshold                 = "1"
   alarm_actions             = ["${var.alert_sns_topic_arn}"]
