a76919006b9af1c85a25b898bf052e2c2db94e44
Oleg Vaskevich <oleg@osv.im>
2026-06-20 10:55:37 -0700

    Narrow scrollspy rootMargin in nav_menu.vue to prevent early transition to Blog

--- root/components/nav_menu.vue
+++ root/components/nav_menu.vue
@@ -52,7 +52,7 @@ onMounted(() => {
   // Setup intersection observer for scrollspy
   const observerOptions = {
     root: null,
-    rootMargin: '-80px 0px -40% 0px',
+    rootMargin: '-80px 0px -80% 0px',
     threshold: 0,
   };
 
